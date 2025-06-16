b76c3afc5fe5f794edd91f4d1cba6c21a65216ae
parthgodhani-dev <72399975+parthgodhani-dev@users.noreply.github.com>
2025-06-16 11:11:23 +0000

    conversion

---
+++ basics/conversionoperator.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/* 
+There are 2 types of conversions:
+#1 : Implicit (automatic by JS) 
+     eg. console.log("5" * 2); // 10
+
+#2 : Explicit (done manually using functions/operators)
+     console.log(Number("123"));     // 123
+*/
+
+/* for number conversion */
+
+// let Scrore = "10abc";
+// console.log(typeof Scrore);
+// let convertToNumber = Number(Scrore);
+// console.log(typeof convertToNumber);
+// console.log(convertToNumber);
+
+/* for string conversion */
+
+// let name = 36;
+// console.log(typeof name); 
+// let convertToString = String(name);
+// console.log(typeof convertToString);
+// console.log(convertToString);
+
+
+/* for boolean conversion */
+
+// let userLoggedIn = true;
+// console.log(typeof userLoggedIn)
+// let convertToNumber = Number(userLoggedIn)
+// console.log(typeof convertToNumber);
+// console.log(convertToNumber);
+
+
