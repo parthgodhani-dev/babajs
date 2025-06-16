aacaeaea8d48a6daf72f5adbaa6902809705ba7a
parthgodhani-dev <72399975+parthgodhani-dev@users.noreply.github.com>
2025-06-16 22:24:25 +0000

    -

---
+++ basics/operators.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/*
+Category	        Examples
+Arithmetic	        +, -, *, /, %, **
+Assignment	        =, +=, -=, *=, /=, **=
+Comparison	        ==, ===, !=, !==, <, >
+Logical	            &&, `
+Unary	            +, -, typeof, !
+Increment/Dec	    ++, --
+Ternary	            condition ? val1 : val2
+Bitwise	            &, `
+Type	            typeof, instanceof
+Object	            in, delete
+*/
+
+// let x = 3;
+// const y = x++
+// console.table(x);
+// console.table(y);
+
+// let a = 3;
+// const b = ++a
+// console.table(a);
+// console.table(b);
+
+
+// Comparison
+
+console.log("2" > 2);
+console.log("2" < 2);
+console.log("2" == 2);
+console.log("2" === 2);
+
+// console.log(null != undefined);
